9b61514bdd4a18b843cdeea57a4672f512f2732f
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-05-11 17:58:06 +0530

    Delete vars/shared-libraries directory

--- vars/shared-libraries/mavenBuildNotify.groovy
+++
@@ -1,6 +0,0 @@
-def call(Map config = [:]) {
-    // Input parameters with defaults
-    def repoUrl = config.repoUrl ?: error("Missing repoUrl")
-    def branch = config.branch ?: 'main'
-    def mvnCommand = config.mvnCommand ?: 'clean package'
-    def notifier = config.notifier ?: 'console' // 'slack', 'telegram', 'console'
